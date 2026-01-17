e6846a57a1a6ae58840dc2679ff23d646b7b5582
VinyBoy <victornieto-juan@live.fr>
2026-01-17 16:08:33 +0100

    Adding git / zsh / curl & ohmyzsh

--- scripts/install_paquet.sh
+++ scripts/install_paquet.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
-need_cmd() { command -v "$1" > dev/null 2>&1;  }
+need_cmd() { command -v "$1" >/dev/null 2>&1; }
 
 install_pkg()
 {
@@ -15,3 +15,21 @@ install_pkg()
         sudo apt-get install -y "$pkg"
     fi
 }
+
+install_pkg git zsh
+install_pkg zsh
+install_pkg curl
+
+# Installer Oh My Zsh pour l'utilisateur actuel
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "✅Installation de Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "Oh My Zsh déjà installé"
+fi
+
+# Changer le shell par défaut
+if [ "$SHELL" != "$(which zsh)" ]; then
+    echo "Changement du shell par défaut vers Zsh..."
+    chsh -s "$(which zsh)"
+fi
